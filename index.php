b740fd702e47efaed097513208f926302dae313f
jinjiuling <65697148+jinjiuling@users.noreply.github.com>
2022-01-14 19:00:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
 $url=$_GET['url'];
-require __DIR__ . '/../gethtml.php';
+require __DIR__ . '/gethtml.php';
  if (strpos($url, 'torrent')){$magnet1=$url;}else{
 $html=gethtml($url);
 preg_match('|href="magnet(.*?)"|i',$html,$magnet);
 $magnet=str_replace('href="','',$magnet[0]);
 $magnet=str_replace('"','',$magnet);
 }
-require __DIR__ . '/../webtor.php';
+require __DIR__ . '/webtor.php';
 ?>
