18104ad44b17c325165c0864837b04cd9e836b62
jinjiuling <65697148+jinjiuling@users.noreply.github.com>
2022-01-14 19:27:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 $url=$_GET['url'];
 require __DIR__ . '/gethtml.php';
- if (strpos($url, 'torrent')){$magnet1=$url;}else{
+ if (strpos($url, '.torrent')){$magnet1=$url;}else{
 $html=gethtml($url);
 preg_match('|href="magnet(.*?)"|i',$html,$magnet);
 $magnet=str_replace('href="','',$magnet[0]);
